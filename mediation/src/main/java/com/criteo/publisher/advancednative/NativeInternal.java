1f3f8e07e90ada161ae334ab01a46ea9d6270a9e
p.turpin <p.turpin@criteo.com>
2020-06-11 10:12:07 +0200

    Disable minification of the NativeInternal class
    Minification should not occur because this class is a bridge between the
    adapter and the SDK package-private classes/methods. To maintain this
    bridge, the package name should be be mangled.
    
    Change-Id: Ib948ded4224472521053edb99661030fd20ea605
    JIRA: 1107

--- mediation/src/main/java/com/criteo/publisher/advancednative/NativeInternal.java
+++ mediation/src/main/java/com/criteo/publisher/advancednative/NativeInternal.java
@@ -1,9 +1,26 @@
 package com.criteo.publisher.advancednative;
 
+import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.annotation.RestrictTo.Scope;
 import java.lang.reflect.Field;
 
 public class NativeInternal {
+
+  /**
+   * This class serves as a bridge between this adapter and the internals (package-private) of the
+   * SDK.
+   * <p>
+   * To access the package-private methods, a Java class should be in the same package. This is why
+   * this class is @Keep to not mangle the package name. Else the package name could become, {@code
+   * com.criteo.a.a} and then the runtime could throw a {@link IllegalAccessError}.
+   */
+  @Keep
+  @RestrictTo(Scope.LIBRARY)
+  public NativeInternal() {
+    // no-op
+  }
 
   public static CriteoNativeRenderer decorateWithAdChoice(@NonNull CriteoNativeRenderer delegate) {
     // TODO Encapsulate in AdChoiceOverlayNativeRenderer
