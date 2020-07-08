ec9c3564b038ffbc555f7209f8628a5d54804b0e
p.turpin <p.turpin@criteo.com>
2020-07-08 16:34:05 +0200

    Migrate interstitial android tests to Kotlin
    JIRA: EE-1179

--- mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubInterstitialAdapterTest.kt
+++ mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubInterstitialAdapterTest.kt
@@ -13,115 +13,110 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+package com.criteo.mediation.mopub
 
-package com.criteo.mediation.mopub;
+import android.content.Context
+import com.criteo.mediation.mopub.MoPubHelper.serverExtras
+import com.criteo.publisher.CriteoUtil.clearCriteo
+import com.criteo.publisher.CriteoUtil.givenInitializedCriteo
+import com.criteo.publisher.TestAdUnits.INTERSTITIAL
+import com.criteo.publisher.mock.MockedDependenciesRule
+import com.mopub.mobileads.CustomEventInterstitial
+import com.mopub.mobileads.MoPubErrorCode.*
+import com.nhaarman.mockitokotlin2.inOrder
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import javax.inject.Inject
 
-import static com.criteo.mediation.mopub.MoPubHelper.serverExtras;
-import static com.criteo.publisher.CriteoUtil.clearCriteo;
-import static com.criteo.publisher.CriteoUtil.givenInitializedCriteo;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+class CriteoMopubInterstitialAdapterTest {
 
-import android.content.Context;
-import androidx.test.core.app.ApplicationProvider;
-import com.criteo.publisher.TestAdUnits;
-import com.criteo.publisher.mock.MockedDependenciesRule;
-import com.mopub.mobileads.CustomEventInterstitial.CustomEventInterstitialListener;
-import com.mopub.mobileads.MoPubErrorCode;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.InOrder;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+  @Rule
+  @JvmField
+  val mockedDependenciesRule = MockedDependenciesRule()
 
-public class CriteoMopubInterstitialAdapterTest {
+  @Inject
+  private lateinit var context: Context
 
-    @Rule
-    public MockedDependenciesRule mockedDependenciesRule = new MockedDependenciesRule();
+  private val localExtras = mutableMapOf<String, Any>()
 
-    private Context context;
+  @Mock
+  private lateinit var listener: CustomEventInterstitial.CustomEventInterstitialListener
 
-    private final Map<String, Object> localExtras = new HashMap<>();
+  private lateinit var adapter: CriteoInterstitialAdapter
 
-    @Mock
-    private CustomEventInterstitialListener listener;
+  private lateinit var adapterHelper: InterstitialAdapterHelper
 
-    private CriteoInterstitialAdapter adapter;
-    private InterstitialAdapterHelper adapterHelper;
+  @Before
+  fun setUp() {
+    MockitoAnnotations.initMocks(this)
+    clearCriteo()
+    adapter = CriteoInterstitialAdapter()
+    adapterHelper = InterstitialAdapterHelper(adapter)
+  }
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        clearCriteo();
+  @Test
+  fun requestInterstitialAdWithEmptyParameters() {
+    val serverExtras = mapOf<String, String>()
 
-        context = ApplicationProvider.getApplicationContext();
+    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
 
-        adapter = new CriteoInterstitialAdapter();
-        adapterHelper = new InterstitialAdapterHelper(adapter);
+    verify(listener).onInterstitialFailed(ADAPTER_CONFIGURATION_ERROR)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun requestInterstitialAdWithNullPublisherId() {
+    val serverExtras = serverExtras(null, "myAdUnit")
+
+    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
+
+    verify(listener).onInterstitialFailed(ADAPTER_CONFIGURATION_ERROR)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun requestBannerAdWithNullAdUnitId() {
+    val serverExtras = serverExtras("cpId", null)
+
+    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
+
+    verify(listener).onInterstitialFailed(MISSING_AD_UNIT_ID)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun givenNotInitializedCriteo_WhenLoadingInterstitialTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    loadValidInterstitial()
+    loadValidInterstitial()
+
+    checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne()
+  }
+
+  @Test
+  fun givenInitializedCriteo_WhenLoadingInterstitialTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    givenInitializedCriteo()
+
+    loadValidInterstitial()
+    loadValidInterstitial()
+
+    checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne()
+  }
+
+  private fun loadValidInterstitial() {
+    adapterHelper.loadInterstitial(INTERSTITIAL, listener)
+    mockedDependenciesRule.waitForIdleState()
+  }
+
+  private fun checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    inOrder(listener) {
+      verify(listener).onInterstitialFailed(NETWORK_NO_FILL)
+      verify(listener).onInterstitialLoaded()
+      verifyNoMoreInteractions()
     }
-
-    @Test
-    public void requestInterstitialAdWithEmptyParameters() {
-        Map<String, String> serverExtras = new HashMap<>();
-
-        adapter.loadInterstitial(context, listener, localExtras, serverExtras);
-
-        verify(listener).onInterstitialFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void requestInterstitialAdWithNullPublisherId() {
-        Map<String, String> serverExtras = serverExtras(null, "myAdUnit");
-
-        adapter.loadInterstitial(context, listener, localExtras, serverExtras);
-
-        verify(listener).onInterstitialFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void requestBannerAdWithNullAdUnitId() {
-        Map<String, String> serverExtras = serverExtras("cpId", null);
-
-        adapter.loadInterstitial(context, listener, localExtras, serverExtras);
-
-        verify(listener).onInterstitialFailed(MoPubErrorCode.MISSING_AD_UNIT_ID);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void givenNotInitializedCriteo_WhenLoadingInterstitialTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() throws Exception {
-        loadValidInterstitial();
-        loadValidInterstitial();
-
-        checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne();
-    }
-
-    @Test
-    public void givenInitializedCriteo_WhenLoadingInterstitialTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() throws Exception {
-        givenInitializedCriteo();
-
-        loadValidInterstitial();
-        loadValidInterstitial();
-
-        checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne();
-    }
-
-    private void loadValidInterstitial() {
-        adapterHelper.loadInterstitial(TestAdUnits.INTERSTITIAL, listener);
-        mockedDependenciesRule.waitForIdleState();
-    }
-
-    private void checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
-        InOrder inOrder = inOrder(listener);
-        inOrder.verify(listener).onInterstitialFailed(MoPubErrorCode.NETWORK_NO_FILL);
-        inOrder.verify(listener).onInterstitialLoaded();
-        inOrder.verifyNoMoreInteractions();
-    }
-
+  }
 }
