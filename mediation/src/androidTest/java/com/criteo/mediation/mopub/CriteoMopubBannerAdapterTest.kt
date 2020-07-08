71451bfe6349f67799a76312d667556c613c0d89
p.turpin <p.turpin@criteo.com>
2020-07-08 10:09:36 +0200

    Migrate banner android tests to Kotlin
    JIRA: EE-1179

--- mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubBannerAdapterTest.kt
+++ mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubBannerAdapterTest.kt
@@ -13,128 +13,129 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+package com.criteo.mediation.mopub
 
-package com.criteo.mediation.mopub;
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.criteo.mediation.mopub.MoPubHelper.localExtras
+import com.criteo.mediation.mopub.MoPubHelper.serverExtras
+import com.criteo.publisher.CriteoBannerView
+import com.criteo.publisher.CriteoUtil
+import com.criteo.publisher.CriteoUtil.clearCriteo
+import com.criteo.publisher.CriteoUtil.givenInitializedCriteo
+import com.criteo.publisher.TestAdUnits
+import com.criteo.publisher.mock.MockedDependenciesRule
+import com.mopub.mobileads.CustomEventBanner
+import com.mopub.mobileads.MoPubErrorCode
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.inOrder
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import java.util.*
+import javax.inject.Inject
 
-import static com.criteo.mediation.mopub.MoPubHelper.localExtras;
-import static com.criteo.mediation.mopub.MoPubHelper.serverExtras;
-import static com.criteo.publisher.CriteoUtil.clearCriteo;
-import static com.criteo.publisher.CriteoUtil.givenInitializedCriteo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+class CriteoMopubBannerAdapterTest {
 
-import android.content.Context;
-import androidx.test.core.app.ApplicationProvider;
-import com.criteo.publisher.CriteoBannerView;
-import com.criteo.publisher.TestAdUnits;
-import com.criteo.publisher.mock.MockedDependenciesRule;
-import com.mopub.mobileads.CustomEventBanner;
-import com.mopub.mobileads.MoPubErrorCode;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.InOrder;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+  @Rule
+  @JvmField
+  val mockedDependenciesRule = MockedDependenciesRule()
 
-public class CriteoMopubBannerAdapterTest {
+  @Inject
+  private lateinit var context: Context
 
-    @Rule
-    public MockedDependenciesRule mockedDependenciesRule = new MockedDependenciesRule();
+  private lateinit var adapter: CriteoBannerAdapter
 
-    private Context context;
+  private lateinit var adapterHelper: BannerAdapterHelper
 
-    private CriteoBannerAdapter adapter;
-    private BannerAdapterHelper adapterHelper;
+  @Mock
+  private lateinit var listener: CustomEventBanner.CustomEventBannerListener
 
-    @Mock
-    private CustomEventBanner.CustomEventBannerListener listener;
+  @Before
+  fun setUp() {
+    MockitoAnnotations.initMocks(this)
+    clearCriteo()
+    adapter = CriteoBannerAdapter()
+    adapterHelper = BannerAdapterHelper(adapter)
+  }
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        clearCriteo();
+  @Test
+  fun requestBannerAdWithEmptyParameters() {
+    val serverExtras = mapOf<String, String>()
+    val localExtras = mapOf<String, Any>()
 
-        context = ApplicationProvider.getApplicationContext();
+    adapter.loadBanner(context, listener, localExtras, serverExtras)
 
-        adapter = new CriteoBannerAdapter();
-        adapterHelper = new BannerAdapterHelper(adapter);
+    verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun requestBannerAdWithNullAdSize() {
+    val serverExtras = serverExtras("cpId", "myAdUnit")
+    val localExtras = mapOf<String, Any>()
+
+    adapter.loadBanner(context, listener, localExtras, serverExtras)
+
+    verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun requestBannerAdWithNullPublisherId() {
+    val serverExtras = serverExtras(null, "myAdUnit")
+    val localExtras = localExtras(320, 50)
+
+    adapter.loadBanner(context, listener, localExtras, serverExtras)
+    verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun requestBannerAdWithNullAdUnitId() {
+    val serverExtras = serverExtras("cpId", null)
+    val localExtras = localExtras(320, 50)
+
+    adapter.loadBanner(context, listener, localExtras, serverExtras)
+
+    verify(listener).onBannerFailed(MoPubErrorCode.MISSING_AD_UNIT_ID)
+    verifyNoMoreInteractions(listener)
+  }
+
+  @Test
+  fun givenNotInitializedCriteo_WhenLoadingBannerTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    loadValidBanner()
+    loadValidBanner()
+
+    checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne()
+  }
+
+  @Test
+  fun givenInitializedCriteo_WhenLoadingBannerTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    givenInitializedCriteo()
+
+    loadValidBanner()
+    loadValidBanner()
+
+    checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne()
+  }
+
+  private fun loadValidBanner() {
+    adapterHelper.loadBanner(TestAdUnits.BANNER_320_50, listener)
+    mockedDependenciesRule.waitForIdleState()
+  }
+
+  private fun checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
+    inOrder(listener) {
+      verify(listener).onBannerFailed(MoPubErrorCode.NETWORK_NO_FILL)
+      verify(listener).onBannerLoaded(any<CriteoBannerView>())
+      verifyNoMoreInteractions()
     }
-
-    @Test
-    public void requestBannerAdWithEmptyParameters() {
-        Map<String, String> serverExtras = new HashMap<>();
-        Map<String, Object> localExtras = new HashMap<>();
-
-        adapter.loadBanner(context, listener, localExtras, serverExtras);
-
-        verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void requestBannerAdWithNullAdSize() {
-        Map<String, String> serverExtras = serverExtras("cpId", "myAdUnit");
-        Map<String, Object> localExtras = new HashMap<>();
-
-        adapter.loadBanner(context, listener, localExtras, serverExtras);
-        verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void requestBannerAdWithNullPublisherId() {
-        Map<String, String> serverExtras = serverExtras(null, "myAdUnit");
-        Map<String, Object> localExtras = localExtras(320, 50);
-
-        adapter.loadBanner(context, listener, localExtras, serverExtras);
-        verify(listener).onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void requestBannerAdWithNullAdUnitId() {
-        Map<String, String> serverExtras = serverExtras("cpId", null);
-        Map<String, Object> localExtras = localExtras(320, 50);
-
-        adapter.loadBanner(context, listener, localExtras, serverExtras);
-
-        verify(listener).onBannerFailed(MoPubErrorCode.MISSING_AD_UNIT_ID);
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void givenNotInitializedCriteo_WhenLoadingBannerTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() throws Exception {
-        loadValidBanner();
-        loadValidBanner();
-
-        checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne();
-    }
-
-    @Test
-    public void givenInitializedCriteo_WhenLoadingBannerTwice_MissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() throws Exception {
-        givenInitializedCriteo();
-
-        loadValidBanner();
-        loadValidBanner();
-
-        checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne();
-    }
-
-    private void loadValidBanner() {
-        adapterHelper.loadBanner(TestAdUnits.BANNER_320_50, listener);
-        mockedDependenciesRule.waitForIdleState();
-    }
-
-    private void checkMissFirstOpportunityBecauseOfBidCachingAndSucceedOnNextOne() {
-        InOrder inOrder = inOrder(listener);
-        inOrder.verify(listener).onBannerFailed(MoPubErrorCode.NETWORK_NO_FILL);
-        inOrder.verify(listener).onBannerLoaded(any(CriteoBannerView.class));
-        inOrder.verifyNoMoreInteractions();
-    }
-
+  }
 }
