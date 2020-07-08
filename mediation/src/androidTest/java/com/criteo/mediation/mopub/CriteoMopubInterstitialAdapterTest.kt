a5dc84217a97ee3f68462bf78b9a43fb9f0d0dfa
p.turpin <p.turpin@criteo.com>
2020-07-08 16:36:42 +0200

    Remove interstitial android tests duplicating the unit tests
    JIRA: EE-1179

--- mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubInterstitialAdapterTest.kt
+++ mediation/src/androidTest/java/com/criteo/mediation/mopub/CriteoMopubInterstitialAdapterTest.kt
@@ -15,34 +15,24 @@
  */
 package com.criteo.mediation.mopub
 
-import android.content.Context
-import com.criteo.mediation.mopub.MoPubHelper.serverExtras
 import com.criteo.publisher.CriteoUtil.clearCriteo
 import com.criteo.publisher.CriteoUtil.givenInitializedCriteo
 import com.criteo.publisher.TestAdUnits.INTERSTITIAL
 import com.criteo.publisher.mock.MockedDependenciesRule
 import com.mopub.mobileads.CustomEventInterstitial
-import com.mopub.mobileads.MoPubErrorCode.*
+import com.mopub.mobileads.MoPubErrorCode.NETWORK_NO_FILL
 import com.nhaarman.mockitokotlin2.inOrder
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
-import javax.inject.Inject
 
 class CriteoMopubInterstitialAdapterTest {
 
   @Rule
   @JvmField
   val mockedDependenciesRule = MockedDependenciesRule()
-
-  @Inject
-  private lateinit var context: Context
-
-  private val localExtras = mutableMapOf<String, Any>()
 
   @Mock
   private lateinit var listener: CustomEventInterstitial.CustomEventInterstitialListener
@@ -57,36 +47,6 @@ class CriteoMopubInterstitialAdapterTest {
     clearCriteo()
     adapter = CriteoInterstitialAdapter()
     adapterHelper = InterstitialAdapterHelper(adapter)
-  }
-
-  @Test
-  fun requestInterstitialAdWithEmptyParameters() {
-    val serverExtras = mapOf<String, String>()
-
-    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
-
-    verify(listener).onInterstitialFailed(ADAPTER_CONFIGURATION_ERROR)
-    verifyNoMoreInteractions(listener)
-  }
-
-  @Test
-  fun requestInterstitialAdWithNullPublisherId() {
-    val serverExtras = serverExtras(null, "myAdUnit")
-
-    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
-
-    verify(listener).onInterstitialFailed(ADAPTER_CONFIGURATION_ERROR)
-    verifyNoMoreInteractions(listener)
-  }
-
-  @Test
-  fun requestBannerAdWithNullAdUnitId() {
-    val serverExtras = serverExtras("cpId", null)
-
-    adapter.loadInterstitial(context, listener, localExtras, serverExtras)
-
-    verify(listener).onInterstitialFailed(MISSING_AD_UNIT_ID)
-    verifyNoMoreInteractions(listener)
   }
 
   @Test
